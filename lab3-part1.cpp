70d3dbd1ad6ca61c91e6f4e4ad8450e3056684eb
HYUN LANG BAN <gusfkd2827@gmail.com>
2019-01-21 14:58:04 -0500

    Add lab3-part1.cpp

---
+++ lab3-part1.cpp
@@ -0,0 +1,36 @@
+// Purpose:  Uses branching to print different messages based on a student's
+//           information.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int credits;
+    bool isCommuter;
+    string major;
+
+    cout << "Enter the number of credit hours you have completed: ";
+    cin >> credits;
+
+    cout << "Enter your commuter status [1=commuter,0=not a commuter]: ";
+    cin >> isCommuter;
+
+    cout << "Enter your major prefix (CSC/BEN/MTH/etc.): ";
+    cin >> major;
+
+    // TODO Add in the questions/solutions here.
+    if(isCommuter){
+        cout<<"Parking must be a pain!"<<endl;
+    }
+
+    if(credits>=30&&credits<=59){
+        cout<<"You have Sophomore status"<<endl;
+    }
+    else if((credits+30)>=30 && (credits+30)<=59){
+        cout<<"You'll be a Sophomroe next year!"<<endl;
+    }
+    if(major=="CSC"||major=="MTH"||major="BEN"||major=="BIO")||major=="ENV"||major=="BUS"){
+        cout<<"You're at home in LSB!"<<endl;
+    }
+
+    return 0;
+}
