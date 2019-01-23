94e9ef977083abc804d63410e07fce51db2551c8
HYUN LANG BAN <gusfkd2827@gmail.com>
2019-01-22 21:06:14 -0500

    Add 2d_array

---
+++ 2d_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int NUM_STUDENTS=3;
+    const int NUM_ASSIGNMENTS=5;
+
+    int scores[NUM_STUDENTS][NUM_ASSIGNMENTS]={
+        {85, 82, 80, 78, 88}, 
+        {90, 95, 92, 87, 93}, 
+        {45, 67, 50, 46, 70}
+        };
+
+    //calculate and display the average assignment score over all
+    for(int i=0; i<NUM_STUDENTS; i++){
+        for(int j=0; j<NUM_ASSIGNMENTS; j++){
+            float sum = 0;
+            sum += scores[i][j];
+        }
+    } 
+
+    float average = sum / (NUM_STUDENTS)*(NUM_ASSIGNMENTS);
+    cout<<"The average: "<<average;
+    return 0;
+}
