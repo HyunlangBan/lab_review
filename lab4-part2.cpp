27a36b8979d997ad8b6a78bcebec94ce6dd79e12
HYUN LANG BAN <gusfkd2827@gmail.com>
2019-01-22 23:57:07 -0500

    Modify lab4-part2.cpp

--- lab4-part2.cpp
+++ lab4-part2.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main(){
     string userInput;
-    int numVowels, numCons, numWords = 0;
+    int numVowels=0, numCons= 0;
+    int numWords=1;
+    float averageOfVowels=0, averageOfCons = 0;
     // reads in a full line of text from the user into a single string variable
     cout<<"Enter a text: "<<endl;
     getline(cin, userInput);
@@ -13,6 +15,7 @@ int main(){
         if(userInput.at(i)=='a'||userInput.at(i)=='e'||userInput.at(i)=='i'||userInput.at(i)=='o'||userInput.at(i)=='u'){
             numVowels++;
         }
+
         else if((userInput.at(i)>=33 && userInput.at(i)<=47)||
         (userInput.at(i)>=58 && userInput.at(i)<=64)||
         (userInput.at(i)>=91 && userInput.at(i)<=96)||
@@ -31,17 +34,12 @@ int main(){
     cout<<"The number of vowels: "<<numVowels<<endl;
     cout<<"The number of consonants: "<<numCons<<endl;
     cout<<"The number of words: "<<numWords<<endl;
-        
-    // the number of vowels (a, e, i, o, u; upper or lower case)
-    // the number of consonants (any character that's not punctuation, a number, or a vowel)
-    // the number of words (e.g., the number of spaces in the string + 1)
-    // HINT: you may find the character functions listed in Ch. 3.15 of the Zybook helpful! Be sure to include the necessary library
-    // display the following stats preceded by a descriptive label (e.g., "Words: 5"):
-    // number of words
-    // total number of vowels
-    // average vowels per word (# of vowels divided by # of words; should display as a floating point)
-    // total number of consonants
-    // average consonants per word (# of consonants / # of words; should display as a floating point)
+
+    averageOfCons = numCons/(numWords*1.0);
+    averageOfVowels = numVowels/(numWords*1.0);
+
+    cout<<"The average of vowels: "<<averageOfVowels<<endl;
+    cout<<"The average of consonants: "<<averageOfCons<<endl;
 
     return 0;
 }
